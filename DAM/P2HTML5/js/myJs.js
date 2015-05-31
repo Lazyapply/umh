8afc6cc3362b6ad4a6ecd4be0e18e1af7eb3d657
Lazyapply <lazyapply@gmail.com>
2015-05-31 17:24:20 +0200

    P2 HTML5 finished

--- DAM/P2HTML5/js/myJs.js
+++ DAM/P2HTML5/js/myJs.js
@@ -18,11 +18,45 @@ function resizeIframe(obj){
 
 function o(valor){ (DEBUG_MODE_ON) ? console.log(valor) : false; }
 
-function CanvasDrawRectangle(objcanvas, x, y, height, width){
+function CanvasDrawRectangle(objcanvas, x, y, height, width, shadow){
 	var ctx = objcanvas.getContext("2d");
-	ctx.rect(x, y, width, height);
-	ctx.strokeStyle = '#ff00ff';
+
+	if(shadow == 1){
+		ctx.shadowOffsetX = 5;
+		ctx.shadowOffsetY = 5;
+		ctx.shadowBlur = 10;
+		ctx.shadowColor = "Brown";
+		ctx.fillStyle = "Gold";
+		ctx.fillRect(x, y, width, height);
+	}
+	else{
+		ctx.rect(x, y, width, height);
+		ctx.strokeStyle = '#ff00ff';
+		ctx.stroke();
+	}
+	o(shadow);
+}
+
+
+
+
+function CanvasDrawArc(objCanvas, cx, cy, r, sa, ea){
+	var ctx = objCanvas.getContext("2d");
+	ctx.beginPath();
+	ctx.arc(cx, cy, r, sa, ea);
+	ctx.strokeStyle = '#000';
 	ctx.stroke();
+}
+
+function CanvasDrawLinearGradient(objCanvas, x1, y1, x2, y2, color1, color2){
+	var ctx = objCanvas.getContext("2d");
+	var grd = ctx.createLinearGradient(0,0,170,0);
+
+	grd.addColorStop(0,color1);
+	grd.addColorStop(1,color2);
+
+	ctx.fillStyle = grd;
+	ctx.fillRect(x1,y1,x2,y2);
 }
 
 function CanvasDrawLine(objcanvas, sx, sy, ex, ey){
@@ -33,7 +67,27 @@ function CanvasDrawLine(objcanvas, sx, sy, ex, ey){
 	ctx.stroke();
 }
 
+
+function CanvasDrawText(objcanvas, x, y, text, font){
+
+	var ctx = objcanvas.getContext("2d");
+
+	if(font != undefined)
+		ctx.font = font;
+	else
+		ctx.font = "20px Georgia"
+
+	ctx.fillStyle = '#000';
+	ctx.fillText(text,x,y);
+
+}
+
 function drawCanvas(objcanvas){
-	CanvasDrawRectangle(objcanvas, 5, 5, 50, 80);
+	CanvasDrawRectangle(objcanvas, 5, 5, 50, 80, 0);
 	CanvasDrawLine(objcanvas, 0, 60, 100, 60);
+	CanvasDrawArc(objcanvas,100,75,50,2,3);
+	CanvasDrawLinearGradient(objcanvas, 120, 20, 150, 50, '#ff00ff', '#5f5f5f');
+	CanvasDrawRectangle(objcanvas, 100, 75, 50, 80, 1);
+	CanvasDrawText(objcanvas, 10, 100, 'texto de prueba');
+	//20,20,150,100
 }
