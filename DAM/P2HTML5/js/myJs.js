97a2d3327702b35eb9dce1392c0223642f0155f5
Lazyapply <lazyapply@gmail.com>
2015-05-31 14:45:43 +0200

    working on canvas & svg, rect, line done

--- DAM/P2HTML5/js/myJs.js
+++ DAM/P2HTML5/js/myJs.js
@@ -1,11 +1,13 @@
 var DEBUG_MODE_ON = 1;
 var iFrAudioPlayer 	= iFrAudioPlayer 	= document.getElementById("iAudioPlayer");
 var iFrVideoPlayer 	= document.getElementById('iVideoPlayer');
+var canvas 			= document.getElementById('myCanvas');
 // var aux 			= setInterval(function(){ini()},200);
 
 function ini(){
 	
 	 resizeIframe(iFrAudioPlayer);
+	 drawCanvas(canvas);
 	 // resizeIframe(iFrVideoPlayer);
 	 // o("cambio");
 }
@@ -16,6 +18,22 @@ function resizeIframe(obj){
 
 function o(valor){ (DEBUG_MODE_ON) ? console.log(valor) : false; }
 
-function CanvasDrawRectangle(){
-	
+function CanvasDrawRectangle(objcanvas, x, y, height, width){
+	var ctx = objcanvas.getContext("2d");
+	ctx.rect(x, y, width, height);
+	ctx.strokeStyle = '#ff00ff';
+	ctx.stroke();
+}
+
+function CanvasDrawLine(objcanvas, sx, sy, ex, ey){
+	var ctx = objcanvas.getContext("2d");
+	ctx.moveTo(sx,sy);
+	ctx.lineTo(ex,ey);
+	ctx.strokeStyle = '#ffaf00';
+	ctx.stroke();
+}
+
+function drawCanvas(objcanvas){
+	CanvasDrawRectangle(objcanvas, 5, 5, 50, 80);
+	CanvasDrawLine(objcanvas, 0, 60, 100, 60);
 }
