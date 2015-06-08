47e94d93823ae280efb02ff0e4cead41bf9e18e8
Lazyapply <lazyapply@gmail.com>
2015-06-08 17:37:59 +0200

    paypal finished

--- CE/js/pasarela.js
+++ CE/js/pasarela.js
@@ -1,45 +1,97 @@
-var carritoArray = array();
-var carritoPrecio = array();
+var carritoArray = [];
+var carritoPrecio = [];
+var carrito = document.getElementById("carrito");
+var carrito_out = document.getElementById("carrito_form");
+var total_price = document.getElementById("totalPrice");
+
 
 function addProduct(product){
 
-	var carrito = document.getElementById("carrito");
 
 
 
 	switch (product){
 
 		case 1:
-			carrito.innerHTML += "<div class=\"col s8 left-align\">Nikon D5300 24MP</div><div class=\"col s4 left-align\">639.00€</div>";
-			carritoArray.push("Nikon D5300 24MP");
-			carritoPrecio.push("639.00");
+
+			if(carritoArray.indexOf("Nikon D5300 24MP") == -1){
+
+				carritoArray.push("Nikon D5300 24MP");
+				carritoPrecio.push(639.00);
+
+				carrito.innerHTML += "<div class=\"col s8 left-align\">Nikon D5300 24MP</div><div class=\"col s4 left-align\">639.00€</div>";
+				carrito_out.innerHTML += "<input type=\"hidden\" name=\"item_name_"+ carritoArray.length +"\" value=\"Nikon D5300 24MP\">";
+				carrito_out.innerHTML += "<input type=\"hidden\" name=\"amount_" + carritoArray.length +"\" value=\"639.00\">";
+			}
 		break;
 
 		case 2:
-			carrito.innerHTML += "<div class=\"col s8 left-align\">Verbatin Portable HHD</div><div class=\"col s4 left-align\">79.95€</div>";
+			if(carritoArray.indexOf("Verbatin Portable HHD") == -1){
+				carritoArray.push("Verbatin Portable HHD");
+				carritoPrecio.push(79.95);
+				carrito.innerHTML += "<div class=\"col s8 left-align\">Verbatin Portable HHD</div><div class=\"col s4 left-align\">79.95€</div>";
+				carrito_out.innerHTML += "<input type=\"hidden\" name=\"item_name_"+ carritoArray.length +"\" value=\"Verbatin Portable HHD\">";
+				carrito_out.innerHTML += "<input type=\"hidden\" name=\"amount_" + carritoArray.length +"\" value=\"79.95\">";
+			}
 		break;
 
 		case 3:
-			carrito.innerHTML += "<div class=\"col s8 left-align\">Sony Xperia M4</div><div class=\"col s4 left-align\">228.00€</div>";
+			if(carritoArray.indexOf("Sony Xperia M4") == -1){
+				carritoArray.push("Sony Xperia M4");
+				carritoPrecio.push(228.00);
+				carrito.innerHTML += "<div class=\"col s8 left-align\">Sony Xperia M4</div><div class=\"col s4 left-align\">228.00€</div>";
+				carrito_out.innerHTML += "<input type=\"hidden\" name=\"item_name_"+ carritoArray.length +"\" value=\"Sony Xperia M4\">";
+				carrito_out.innerHTML += "<input type=\"hidden\" name=\"amount_" + carritoArray.length +"\" value=\"228.00\">";
+			}
 		break;
 	
 		case 4:
-			carrito.innerHTML += "<div class=\"col s8 left-align\">MSI GeForce Gtx 960</div><div class=\"col s4 left-align\">270.00€</div>";	
+			if(carritoArray.indexOf("MSI GeForce Gtx 960") == -1){
+				carritoArray.push("MSI GeForce Gtx 960");
+				carritoPrecio.push(270.00);
+				carrito.innerHTML += "<div class=\"col s8 left-align\">MSI GeForce Gtx 960</div><div class=\"col s4 left-align\">270.00€</div>";	
+				carrito_out.innerHTML += "<input type=\"hidden\" name=\"item_name_"+ carritoArray.length +"\" value=\"MSI GeForce Gtx 960\">";
+				carrito_out.innerHTML += "<input type=\"hidden\" name=\"amount_" + carritoArray.length +"\" value=\"270.00\">";
+			}
 		break;
 
 	}
-	alert(carritoArray);
+
 	addTotal();
 }
 
 function addTotal(){
-	alert("entro");
+	
+	
+
 	 arrayAux = carritoArray.slice();
 	 arrayPrecio = carritoPrecio.slice();
-	 total = arrayAux.lenght;
+	 var precioTotal = 0.0;
+	 var totalField = document.getElementById("total");
+	 var i = 0;
+	 total = carritoArray.length;
 
-	alert(carritoArray);
-	var totalField = document.getElementById("total");
+	 //alert(carritoArray.length);
+	 console.log("------------------------");
+	 for(i=0;i<total;i++){
+	 	console.log(carritoArray[i] + " ->" + carritoPrecio[i]);
+	 	precioTotal += carritoPrecio[i];
+	 }
+
+	 total_price.innerHTML = precioTotal + " €";
+	 //carrito_out.innerHTML += "<input type=\"hidden\" name=\"item_name_1" value="Compra de Producto X">";
+			
+
+			/*
+							<input type="hidden" name="item_name_1" value="Compra de Producto X">
+				<input type="hidden" name="amount_1" value="50.00">
+
+			 */
+
+
+
+	//alert(carritoArray.length);
+	
 
 
 
